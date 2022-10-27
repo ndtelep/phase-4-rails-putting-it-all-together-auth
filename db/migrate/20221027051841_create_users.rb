31db53f73c5ff9a4aa52d169b9d03564b4b0b6f1
Nicolas Telep <ndtelep@gmail.com>
2022-10-26 23:18:54 -0600

    commit tree 0d9c1cffb7543a0a3f61d8cee850b909342f7238

---
+++ db/migrate/20221027051841_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :password_digest
+      t.string :image_url
+      t.string :bio
+
+      t.timestamps
+    end
+  end
+end
